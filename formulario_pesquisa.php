758bd837f790ebf618a7e02bd51338fd79089dfd
elton023 <159277209+elton023@users.noreply.github.com>
2024-06-19 22:00:02 -0300

    fiz formulario pesquisa

---
+++ formulario_pesquisa.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pesquisa Cliente</title>
+</head>
+<body>
+    <h1>Pesquisa Cliente</h1>
+    <form method="GET" action="pesquisa.php">
+        <label for="termo">Digite uma palavra da pesquisa:</label>
+        <input type="text" id="termo" name="termo"><br><br>
+        <input type="submit" value="Pesquisar">
+    </from>
+    
+    
+</body>
+</html>
---
+++ pesquisa.php
@@ -0,0 +1,39 @@
+<?php
+//conexao com banco de dados
+
+$servername = "localhost";
+$username = "root";
+$password ="";
+$dbname = "bd_cadastro";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error){
+    die("Erro na conexao com o banco de dados:" . $conn->connect_error);
+    }
+
+$termo = $_GET["termo"];
+$sql = "SELECT * FROM cliente WHERE nome LIKE '%termo%' OR email LIKE '%termo%' OR telefone LIKE '%termo%' OR descricao LIKE '%termo%'"; 
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0){
+    echo "<h2>Resultados da Pesquisa</h2>";
+    echo "<table border='1'";
+    echo "<tr><th>Email</th<th>Telefone</th<th>Descrição</th></tr>";
+    while($row = $result->fetch_assoc()){
+        echo "<tr><td>".$row["nome"]."<tr><td>".$row["email"]."<tr><td>".$row["telefone"]."<tr><td>".$row["descricao"]."</td></tr>";
+    }
+}else{
+    echo "Nenhum resultado encontrado.";
+}
+$conn->close();
+
+
+
+
+
+
+
+
+?>
